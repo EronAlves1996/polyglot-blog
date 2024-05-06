3e1970c93128c44fa641488b4b94e66687c5d2ed
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-05 23:25:26 -0300

    feat: declare search page handler

--- main.go
+++ main.go
@@ -7,24 +7,59 @@ import (
 
 var hostPort = ":4000"
 
+type HandlerMatcher struct {
+	handlers map[string]func(http.ResponseWriter, *http.Request)
+}
+
+func (hm *HandlerMatcher) register(method string,
+	handler func(http.ResponseWriter, *http.Request)) {
+	hm.handlers[method] = handler
+}
+
+func (hm *HandlerMatcher) tryMatch(w http.ResponseWriter,
+	r *http.Request) {
+	handler := hm.handlers[r.Method]
+	if handler == nil {
+		http.Error(w, "Method not Allowed", 405)
+		return
+	}
+	handler(w, r)
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.String() != "/" {
 		http.Error(w, "Not Found!", 404)
 		return
 	}
 
-	if r.Method != http.MethodGet {
-		w.Header().Set("Allow", http.MethodGet)
-		http.Error(w, "Method not Allowed", 405)
-		return
+	hm := HandlerMatcher{
+		handlers: make(map[string]func(http.ResponseWriter, *http.Request)),
 	}
 
-	w.Write([]byte("Placeholder for home page"))
+	hm.register(http.MethodGet, func(wr http.ResponseWriter,
+		r *http.Request) {
+		w.Write([]byte("Placeholder for home page"))
+	})
+
+	hm.tryMatch(w, r)
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	hm := HandlerMatcher{
+		handlers: make(map[string]func(http.ResponseWriter, *http.Request)),
+	}
+
+	hm.register(http.MethodGet, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Placheolder for search page"))
+	})
+
+	hm.tryMatch(w, r)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/search", search)
 
 	err := http.ListenAndServe(hostPort, mux)
 	log.Fatal(err)

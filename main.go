a62ae08b423f6549cae0ad8f1de7c73727d757d4
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-05 23:45:52 -0300

    feat: blog post endpoints

--- main.go
+++ main.go
@@ -56,10 +56,40 @@ func search(w http.ResponseWriter, r *http.Request) {
 	hm.tryMatch(w, r)
 }
 
+func viewPost(w http.ResponseWriter, r *http.Request) {
+	if r.URL.String() != "/post" {
+		http.Error(w, "Not Found!", 404)
+		return
+	}
+
+	hm := HandlerMatcher{
+		handlers: map[string]func(http.ResponseWriter, *http.Request){},
+	}
+
+	hm.register(http.MethodGet, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Placholder for blog post page"))
+	})
+
+	hm.register(http.MethodPost, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Placeholder for creating blog post page"))
+	})
+
+	hm.register(http.MethodPut, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Placeholder for editing a blog post"))
+	})
+
+	hm.register(http.MethodDelete, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Placeholder for deleting a blog post"))
+	})
+
+	hm.tryMatch(w, r)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/search", search)
+	mux.HandleFunc("/post/", viewPost)
 
 	err := http.ListenAndServe(hostPort, mux)
 	log.Fatal(err)
